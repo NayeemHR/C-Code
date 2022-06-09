c351e4b914ff1719b03a41959bcca0ec306241a7
Nayeem HR <nayeemhasan042@yahoo.com>
2022-06-09 08:07:34 +0600

    Create First and Second Largest Number.c

---
+++ First and Second Largest Number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int n, i, temp, arr[100];
+
+    printf("Enter the number of elements:");
+    scanf("%d", &n);
+
+    for(i=0;i<n;i++){
+        scanf("%d", &arr[i]);
+    }
+    for(i=0;i<n;i++){
+        if(arr[0] < arr[i]){
+            temp = arr[0];
+            arr[0] = arr[i];
+        }else if(temp<arr[i] && arr[0] > arr[i]){
+            temp = arr[i];
+        }
+    }
+    printf("First largest element = %d", arr[0]);
+    printf("Second largest element = %d", temp);
+
+}
